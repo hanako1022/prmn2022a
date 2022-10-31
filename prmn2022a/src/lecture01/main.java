57453137d4b2d78afe07e43f8255545a1a4fb1cd
hanako1022 <b2210240@photon.chitose.ac.jp>
2022-10-31 18:06:01 +0900

    Github上に表示するコメント文(例:プロジェクト作成)

--- prmn2022a/src/lecture01/main.java
+++ prmn2022a/src/lecture01/main.java
@@ -3,6 +3,10 @@ package lecture01;
 public class main {
     public static void main(String[] args) {
         System.out.println("B2210240 ito haru");
+
+        int studentNumber = 2210240;
+
+        System.out.println("B"+ studentNumber +" ito haru");
     }
 
 
