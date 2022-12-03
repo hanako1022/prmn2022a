e8aa901f6706afbbf32dcdac26d4c69adcb232fc
hanako1022 <b2210240@photon.chitose.ac.jp>
2022-12-03 23:22:17 +0900

    lecture06

--- prmn2022a/src/lecture06/Exercise6_2.java
+++ prmn2022a/src/lecture06/Exercise6_2.java
@@ -1,5 +1,6 @@
 package lecture06;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -24,6 +25,9 @@ public class Exercise6_2 {
 
         }catch (IndexOutOfBoundsException e){
             System.out.println("ArrayListの範囲外アクセスを確認しました");
+            System.out.println("プログラムを終了します");
+        }catch(InputMismatchException e){
+            System.out.println("整数以外の値が入力されました");
             System.out.println("プログラムを終了します");
         }
     }
